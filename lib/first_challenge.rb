bddeeb455deed2205fe8d85c0ca42765feb654cb
atkinsonholly <atkinsonholly@gmail.com>
2018-11-11 22:00:18 +0000

    Manipulating hashes tests passing

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,10 @@
+#require 'pry'
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -12,11 +14,6 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|flavor| flavor == "strawberry"}
   contacts
 end
-
